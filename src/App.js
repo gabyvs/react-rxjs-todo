845cd6d38207dc87a4016489f9187cbddb16f482
Gaby Vazquez <gabs.vz@gmail.com>
2019-02-08 15:18:14 -0800

    index tiene funciones

--- src/App.js
+++ src/App.js
@@ -1,76 +1,17 @@
 import React, { Component } from 'react';
-import { Subject }          from 'rxjs';
-import {scan} from 'rxjs/operators';
 import './App.css';
 import AddTodo              from './AddTodo';
 import TodoList             from './TodoList';
 
-let yesterday = new Date();
-yesterday.setDate(yesterday.getDate() - 1);
-
-const addTodo = text => state => {
-  return {
-    ...state,
-    todos: [...state.todos, {
-      completed: false,
-      text,
-      id: new Date()
-    }]
-  };
-};
-
-const removeTodo = id => state => {
-  return {
-    ...state,
-    todos: state.todos.filter((todo) => todo.id !== id)
-  };
-};
-
-const toggleTodo = id => state => {
-  return {
-    ...state,
-    todos: state.todos.map((todo) => {
-      if (todo.id === id) return { ...todo, completed: !todo.completed };
-      return todo;
-    })
-  };
-};
-
 class App extends Component {
-  state = {
-    todos: [{
-      completed: false,
-      text: 'Finish app',
-      id: new Date()
-    }, {
-      completed: true,
-      text: 'Start app',
-      id: yesterday
-    }]
-  };
-
-  store = new Subject();
-
-  componentDidMount () {
-    this.store
-      .pipe(scan((acc, f) => f(acc), this.state))
-      .subscribe(
-        state => this.setState(state)
-      );
-  }
-
-  addTodo = text => this.store.next(addTodo(text));
-  removeTodo = id => this.store.next(removeTodo(id));
-  toggleTodo = id => this.store.next(toggleTodo(id));
-
   render() {
     return (
       <div className='App'>
-        <AddTodo addTodo={this.addTodo}/>
+        <AddTodo addTodo={this.props.addTodo}/>
         <TodoList
-          todos={this.state.todos}
-          removeTodo={this.removeTodo}
-          toggleTodo={this.toggleTodo}/>
+          todos={this.props.todos}
+          removeTodo={this.props.removeTodo}
+          toggleTodo={this.props.toggleTodo}/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React     from 'react';
+import ReactDOM  from 'react-dom';
 import './index.css';
-import App from './App';
+import App       from './App';
+import {Subject} from 'rxjs';
+import {scan} from 'rxjs/operators';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+
+const addTodo = text => state => {
+  return {
+    ...state,
+    todos: [...state.todos, {
+      completed: false,
+      text,
+      id: new Date()
+    }]
+  };
+};
+
+const removeTodo = id => state => {
+  return {
+    ...state,
+    todos: state.todos.filter((todo) => todo.id !== id)
+  };
+};
+
+const toggleTodo = id => state => {
+  return {
+    ...state,
+    todos: state.todos.map((todo) => {
+      if (todo.id === id) return { ...todo, completed: !todo.completed };
+      return todo;
+    })
+  };
+};
+
+const initialState = {
+  todos: [{
+    completed: false,
+    text: 'Finish app',
+    id: new Date()
+  }, {
+    completed: true,
+    text: 'Start app',
+    id: yesterday
+  }]
+};
+
+const store = new Subject();
+
+store
+  .pipe(scan((acc, f) => f(acc), initialState))
+  .subscribe(
+    state => ReactDOM.render(<App
+      todos={state.todos}
+      addTodo={text => store.next(addTodo(text))}
+      removeTodo={id => store.next(removeTodo(id))}
+      toggleTodo={id => store.next(toggleTodo(id))}/>, document.getElementById('root'))
+  );
+
+store.next(x => x);
