2d4afa3264962ba990496f4b354ca368557e0313
Gaby Vazquez <gabs.vz@gmail.com>
2019-02-08 14:29:59 -0800

    Presentational components

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,20 +1,13 @@
-import React, { Component } from 'react';
-import './Todo.css';
+import React from 'react';
 
-class Todo extends Component {
-  render() {
-    const { todo, toggleTodo } = this.props;
-
-    return (
-      <div className="todo">
-        <input type="checkbox"
-               className="todo-check"
-               checked={ todo.completed }
-               onChange={() => toggleTodo(todo.id)}/>
-        <span className={ todo.completed ? 'todo-completed todo-text' : 'todo-text' }>{todo.text}</span>
-      </div>
-    );
-  }
-}
+const Todo = ({ todo, toggleTodo }) => (
+  <div className="todo">
+    <input type="checkbox"
+           className="todo-check"
+           checked={ todo.completed }
+           onChange={() => toggleTodo(todo.id)}/>
+    <span className={ todo.completed ? 'todo-completed todo-text' : 'todo-text' }>{todo.text}</span>
+  </div>
+);
 
 export default Todo;
